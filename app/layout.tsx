2bc295d3cdfa41bbbf5674877d3ca597cadf4cc5
Neil Shah <neildshah1013@gmail.com>
2025-03-31 13:33:07 -0400

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,14 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import { icons } from 'react-icons'
 
 export const metadata: Metadata = {
   title: 'S&Y Consulting',
   description: 'Strategic Solutions for Local Excellence',
   generator: 'v0.dev',
+  icons: {
+    icon: '/favicon.ico',
+  }
 }
 
 export default function RootLayout({
